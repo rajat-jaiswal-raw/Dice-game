07b09be0591357388932c9006d11663d244cc81b
rajat-jaiswal-raw <rajat.jaiswal@raweng.com>
2019-08-08 18:12:04 +0530

    add random roll

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import Die from './Die';
 
 class RollDice extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      die1: 1,
+      die2: 1,
+    }
+    this.faceChange = this.faceChange.bind(this);
+    this.getRandFace = this.getRandFace.bind(this);
+  }
+  getRandFace(){
+    return Math.floor(Math.random() * 6) + 1;
+  }
+  faceChange(){
+    this.setState({
+      die1: this.getRandFace(),
+      die2: this.getRandFace()
+    })
+    console.log(this.state);
+  }
   render(){
     return (
       <div>
-        <Die face={4} />
-        <Die face={3} />
+        <Die face={this.state.die1} />
+        <Die face={this.state.die2} />
+        <button onClick={this.faceChange}>Roll Dice</button>
       </div>
     );
   }
