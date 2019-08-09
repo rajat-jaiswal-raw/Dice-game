3147378404c154ab6890026503f45408f0664296
rajat-jaiswal-raw <rajat.jaiswal@raweng.com>
2019-08-09 14:26:49 +0530

    final commit

--- src/RollDice.js
+++ src/RollDice.js
@@ -8,20 +8,35 @@ class RollDice extends Component {
     this.state = {
       die1: 1,
       die2: 1,
+      rolling: false
     }
     this.faceChange = this.faceChange.bind(this);
     this.getRandFace = this.getRandFace.bind(this);
+    this.roll = this.roll.bind(this);
   }
   getRandFace(){
     return Math.floor(Math.random() * 6) + 1;
   }
+
   faceChange(){
     this.setState({
       die1: this.getRandFace(),
-      die2: this.getRandFace()
-    })
-    console.log(this.state);
+      die2: this.getRandFace(),
+    });
   }
+
+  roll(){
+    this.setState({rolling:true});
+    let waitTime = 0;
+    for(let count = 1; count <= 5; count++){
+      waitTime += 200;
+      setTimeout(this.faceChange, waitTime);
+    }
+    setTimeout(() => {
+      this.setState({rolling:false});
+    }, waitTime);
+  }
+
   render(){
     return (
       <div className="RollDice">
@@ -29,7 +44,9 @@ class RollDice extends Component {
           <Die face={this.state.die1} />
           <Die face={this.state.die2} />
         </div>
-        <button onClick={this.faceChange}>Roll Dice</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling? "Rolling...": "Roll Dice!"}
+        </button>
       </div>
     );
   }
